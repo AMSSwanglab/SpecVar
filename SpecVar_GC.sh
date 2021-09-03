cb8583d1df0821b37cba391c8d3f47b9091389ec
Zhanying Feng <89908600+fengzhanying@users.noreply.github.com>
2021-09-03 14:00:09 +0800

    Update SpecVar_GC.sh

--- SpecVar_GC.sh
+++ SpecVar_GC.sh
@@ -1,13 +1,13 @@
 #/bin/bash
 
 # Genetic Correlation version of SpecVar 1.0, updated at Sep 9th, 2021 by Zhanying Feng
-
+LDSC_HOME='~/software/ldsc'
 #Step 1: Estimating traits' relevance to 77 human contexts
 
 GWAS1=$1;GWAS2=$2
 echo "Estimating "${GWAS1}"'s relevance to 77 human contexts"
-python ~/software/ldsc/munge_sumstats.py --sumstats ./Input/${GWAS1}.txt --merge-alleles ./Data/w_hm3.snplist --out ./Input/${GWAS1} --a1-inc
-python ~/software/ldsc/ldsc.py --h2 ./Input/${GWAS1}.sumstats.gz --ref-ld-chr ./Data/SpecVar/SpecVar. --w-ld-chr ./Data/weights/weights. --overlap-annot --frqfile-chr ./Data/Frq/1000G.EAS.QC. --out ./Results/${GWAS1}.SpecVar
+python $LDSC_HOME/munge_sumstats.py --sumstats ./Input/${GWAS1}.txt --merge-alleles ./Data/w_hm3.snplist --out ./Input/${GWAS1} --a1-inc
+python $LDSC_HOME/ldsc.py --h2 ./Input/${GWAS1}.sumstats.gz --ref-ld-chr ./Data/SpecVar/SpecVar. --w-ld-chr ./Data/weights/weights. --overlap-annot --frqfile-chr ./Data/Frq/1000G.EAS.QC. --out ./Results/${GWAS1}.SpecVar
 cat ./Results/${GWAS1}.SpecVar.results | sed '1,2d' | sed 's/_0//g' | grep -v HumanBrain | awk -v OFS='\t' '{print $1,-$5*log($7)/log(10)}' > ./Results/${GWAS1}.SpecVar.RS.txt
 rm -f ./Results/${GWAS1}.SpecVar.log
 sed s/GWAS/${GWAS1}/g ./scr/GetRevTissue.py > GetRevTissue.py
@@ -15,8 +15,8 @@ python3 GetRevTissue.py
 rm -f GetRevTissue.py
 
 echo "Estimating "${GWAS2}"'s relevance to 77 human contexts"
-python ~/software/ldsc/munge_sumstats.py --sumstats ./Input/${GWAS2}.txt --merge-alleles ./Data/w_hm3.snplist --out ./Input/${GWAS2} --a1-inc
-python ~/software/ldsc/ldsc.py --h2 ./Input/${GWAS2}.sumstats.gz --ref-ld-chr ./Data/SpecVar/SpecVar. --w-ld-chr ./Data/weights/weights. --overlap-annot --frqfile-chr ./Data/Frq/1000G.EAS.QC. --out ./Results/${GWAS2}.SpecVar
+python $LDSC_HOME/munge_sumstats.py --sumstats ./Input/${GWAS2}.txt --merge-alleles ./Data/w_hm3.snplist --out ./Input/${GWAS2} --a1-inc
+python $LDSC_HOME/ldsc.py --h2 ./Input/${GWAS2}.sumstats.gz --ref-ld-chr ./Data/SpecVar/SpecVar. --w-ld-chr ./Data/weights/weights. --overlap-annot --frqfile-chr ./Data/Frq/1000G.EAS.QC. --out ./Results/${GWAS2}.SpecVar
 cat ./Results/${GWAS2}.SpecVar.results | sed '1,2d' | sed 's/_0//g' | grep -v HumanBrain | awk -v OFS='\t' '{print $1,-$5*log($7)/log(10)}' > ./Results/${GWAS2}.SpecVar.RS.txt
 rm -f ./Results/${GWAS2}.SpecVar.log
 sed s/GWAS/${GWAS2}/g ./scr/GetRevTissue.py > GetRevTissue.py
