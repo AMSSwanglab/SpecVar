f6c9c4a7e78c07d2aeb4ecb40a81473f9ff3873e
AMSSwanglab <44538141+AMSSwanglab@users.noreply.github.com>
2023-05-01 16:54:04 -0700

    Add files via upload

--- scr/GetGC.py
+++ scr/GetGC.py
@@ -11,5 +11,5 @@ b = [float(b[i].split('\t')[1]) for i in range(len(b))]
 results = stats.spearmanr(a,b)
 
 g = open('./Results/GWAS1_GWAS2_Corr.txt','w')
-g.write("Genetic correlation: "str(results[0])+'\nP-value: '+str(results[1])+'\n')
+g.write("Genetic correlation: "+str(results[0])+'\nP-value: '+str(results[1])+'\n')
 g.close()
