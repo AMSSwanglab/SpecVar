62604f00308689534fcc4cf98478346faa2ce0f4
AMSSwanglab <44538141+AMSSwanglab@users.noreply.github.com>
2023-05-01 10:02:27 -0700

    Add files via upload

---
+++ SpecVar_FE.sh
@@ -0,0 +1,12 @@
+
+GWAS=$1
+mkdir ${GWAS}_RE_Overlap
+for i in `cat ./Data/RELength.txt | awk '{print $1}'`
+do
+	bedtools intersect -wa -wb -a ./Input/${GWAS}.bed -b ./Data/PECA_Peaks/${i}.bed > ${GWAS}_RE_Overlap/${GWAS}_${i}.txt
+	num=`bedtools intersect -wa -wb -a ./Input/${GWAS}.bed -b ./Data/PECA_Peaks/${i}.bed | awk '{print $4}' | sort | uniq | wl`
+	echo -e -n ${i}"\t"${num}"\n" >> ${GWAS}_RE_Overlap.txt
+done
+sed s/GWAS/${GWAS}/g ./scr/GetFE.py > GetFE.py
+mkdir ./Results/${GWAS}
+python3 GetFE.py;rm -rf ${GWAS}_RE_Overlap ${GWAS}_RE_Overlap.txt GetFE.py
