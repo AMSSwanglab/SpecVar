84d6d5930115e23257ad2853d3e3bd6b34f8de32
AMSSwanglab <44538141+AMSSwanglab@users.noreply.github.com>
2023-05-01 10:04:53 -0700

    Add files via upload

---
+++ scr/GetFE.py
@@ -0,0 +1,18 @@
+	
+FE = [[],[]]	
+L = f.readlines();f.close()	
+N = len(a)	
+a = f.readlines();f.close()	,
+celltype = f.readlines();f.close()	
+del celltype[0]	
+else:	
+f = open('./Data/RELength.txt')	
+f = open('./Input/GWAS.bed')	
+f = open('./Results/GWAS/GWAS_FE.txt')	
+f = open('GWAS_RE_Overlap.txt')	
+for i in range(len(L)):	
+for i in range(len(a)):	
+for i in range(len(celltype)):	
+g = open('./Results/GWAS/GWAS_FE.txt','w')	
+g.write("Contexts\tFE\n")	
+if max(FE[1]) >= 3:	
